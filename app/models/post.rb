92e6a8269dd9ea82af9e106cf2a9b8f0139c52fe
suzu784 <s.suzuki784@hotmail.com>
2026-08-26 18:56:36 +0900

    問題 04

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :users
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 1000 }
