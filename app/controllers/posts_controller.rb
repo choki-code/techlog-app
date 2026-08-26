6fab880633fb1d2da4921c4656e0c453160973c8
suzu784 <s.suzuki784@hotmail.com>
2026-08-26 18:56:36 +0900

    問題 02

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @posts = Post.limit(10).order(created_at: :desc)
+    @post = Post.limit(10).order(created_at: :desc)
   end
 
   def show
